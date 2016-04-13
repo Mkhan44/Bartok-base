ce16b08fc8ba635dfa2571193749939c1b8a0db0
Mohamed R. Khan <khanantisora@hotmail.com>
2016-04-13 00:36:39 -0400

    yay
    yay

--- Assets/__Scripts/Bartok.cs
+++ Assets/__Scripts/Bartok.cs
@@ -14,7 +14,11 @@ public class Bartok : MonoBehaviour {
 	public Deck deck;
 	public List<CardBartok> drawPile;
 	public List<CardBartok> discardPile;
-	
+
+	public BartokLayout layout;
+	public Transform layoutAnchor;
+
+
 	void Awake() {
 		S = this;
 	}
@@ -26,6 +30,20 @@ public class Bartok : MonoBehaviour {
 		deck.InitDeck (deckXML.text); //Pass DeckXML to it.
 		Deck.Shuffle (ref deck.cards); //This shuffles the deck.
 		//The ref keyword passes a reference to deck.cards, which allows deck.cards to be modified by Deck.Shuffle()
+		layout = GetComponent<BartokLayout> (); //Get the layout.
+		layout.ReadLayout (layoutXML.text); //Pass LayoutXML to it.
+
+		drawPile = UpgradeCardsList (deck.cards);
+	}
+
+	//UpgradeCardsList casts the cards in 1CD to be CardBartoks
+	//Of course, they were all along, but this lets Unity know it.
+	List<CardBartok> UpgradeCardList(List<Card> lCD) {
+		List<CardBartok> lCB = new List<CardBartok>();
+		foreach(Card tCD in lCD) {
+			lCB.Add (tCD as CardBartok);
+		}
+		return(lCB);
 	}
 	
 }
--- Assets/__Scripts/BartokLayout.cs
+++ Assets/__Scripts/BartokLayout.cs
@@ -21,6 +21,80 @@ public class SlotDef{
 
 public class BartokLayout : MonoBehaviour {
 
+	public PT_XMLReader xmlr; //Just like deck, this has an PT_XMLReader.
+	public PT_XMLHashtable xml; //This variable is for faster xml access.
+	public Vector2 multiplier; //Sets the spacing of the tableau.
+	//SlotDef references.
+	public List<SlotDef> SlotDefs; //The SlotDefs hands.
+	public SlotDef drawPile;
+	public SlotDef discardPile;
+	public SlotDef target;
+
+	//This function is called to read in the LayoutXML.xml file.
+	public void ReadLayout(string xmlText) {
+		xmlr = new PT_XMLReader ();
+		xmlr.Parse (xmlText); //The XML is parsed.
+		xml = xmlr.xml ["xml"] [0]; //And xml is set as a shortcut to the XML.
+
+		//Read in the multiplier , which sets card spacing.
+		multiplier.x = float.Parse (xml ["multiplier"] [0].att ("x"));
+		multiplier.y = float.Parse (xml ["multiplier"] [0].att ("y"));
+
+		//Read in the slots.
+		SlotDef tSD;
+		//slotsX is used as a shortcut to all the <slot>
+		PT_XMLHashList slotX = xml ["slot"];
+
+		for(int i=0; i<slotsX.Count; i++) {
+			tSD = new SlotDef(); //Create a new SlotDef instance.
+			if(slotsX[i].HasAtt("type")) {
+			} else {
+				//If not, set its type to "slot"
+				tSD.type = "slot";
+			}
+			//Various attributes are parsed into numerical values.
+			tSD.x = float.Parse (slotsX[i].att("x"));
+			tSD.y = float.Parse (slotsX[i].att("y"));
+			tSD.pos = new Vector3(tSD.x*multiplier.x, tSD.y*multiplier.y, 0);
+
+			//Sorting Layers
+			tSD.layerID = int.Parse (slotsX[i].att("layer"));
+			//In this game, the sorting layers are named 1, 2 ,3 ...through 10.
+			//This converts the number of the layerID into a text layerName.
+			tSD.layerName = tSD.layerID.ToString ();
+			//The layers are used to make sure that the correct cards are on top of the others.
+			//In Unity 2D, all of our assets are effectivley at the same Z depth, so sorting layers
+			//are used to differentiate between them.
+			//pull additional attributes based on the type of each <slot>.
+			switch(tSD.type) {
+			case "slot":
+				//ignore slots that are just of the "slot" type.
+				break;
+
+			case "drawpile":
+				//The drawPile xstagger is read but not actually used in bartok.
+				tSD.stagger.x = float.Parse (slotsX[i].att("xstagger"));
+				drawPile = tSD;
+				break;
+
+			case "discardpile":
+				discardPile = tSD;
+				break;
+
+			case "target":
+				//The target card has a diff layer from discardPile.
+				target = tSD;
+				break;
+
+			case "hand":
+				//Information for each player's hand 
+				tSD.player = int.Parse (slotsX[i].att("player"));
+				tSD.rot = float.Parse (slotsX[i].att("rot"));
+				slotDefs.Add(tSD);
+				break;
+			}
+		}
+	}
 	// Use this for initialization
 	void Start () {
 	
